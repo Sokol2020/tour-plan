2cd8556db78cf440a6f370efcf1b9e1a9d3a16cd
Sokol2020 <72626933+Sokol2020@users.noreply.github.com>
2020-11-30 15:50:59 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var hotelSlider = new Swiper('.hotel-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+});
+
+var mySwiper = new Swiper('.hotel-slider', {
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
+
+var reviewSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button-next',
+    prevEl: '.reviews-slider__button-prev',
+  },
+});
+
+var mySwiper = new Swiper('.reviews-slider', {
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
+
+
+// ymaps.ready(init);
+//    function init() {
+//      var myMap = new ymaps.Map("map", {
+ //       center: [27.8555, 34.313955],
+ //       zoom: 17
+ //     });
+//    }
+
