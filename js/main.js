ba7baadd94f38dc888f3b10b3b84ae567b6cfc97
Sokol2020 <72626933+Sokol2020@users.noreply.github.com>
2020-12-05 21:21:38 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -45,3 +45,10 @@ var mySwiper = new Swiper('.reviews-slider', {
  //     });
 //    }
 
+
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener('click', function() {
+  console.log("Клик");
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+});
