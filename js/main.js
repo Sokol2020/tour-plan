54a62a24e042363cf17637c3be4aad92c96f5258
Sokol2020 <72626933+Sokol2020@users.noreply.github.com>
2020-12-09 16:07:55 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -78,5 +78,26 @@ var mySwiper = new Swiper('.reviews-slider', {
   }
 
 
-
+  //Обработка форм
+  $(".form").each(function(){
+    $(this).validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+        required: "Please specify your name",
+        minlength: "The name must be at least two letters",
+      },
+      email: {
+        required: "We need your email address to contact",
+        email: "Your email address must be in the format of name@domain.com",
+      },
+      phone: {
+        required: "We need your phone",
+        //phone: "Your phone must be in the format of +99999999999"
+      },
+    },
+  });
+    //$('.phone').mask('+7(999) 999-99-99');
+    
+  });
 });
